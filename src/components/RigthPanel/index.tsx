0633f60a1cb8018341fa5662c6f1e0c3564345d6
v_zhiyqin <v_zhiyqin@tencent.com>
2022-07-09 20:02:22 +0800

    feat: add 测试

--- src/components/RigthPanel/index.tsx
+++ src/components/RigthPanel/index.tsx
@@ -3,7 +3,7 @@ import './index.less';
 function RightPanel() {
   return (
     <div className="right-panel">
-      <div className="header">右侧属性面板</div>
+      <div className="header">右侧属性</div>
     </div>
   );
 }
